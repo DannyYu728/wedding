e37dc4a34c17b352309fa24088801f3e1d7bf11f
Danny Yu <tiramisu@new-host-3.home>
2025-05-07 04:04:28 -0400

    config,session,pydantic_setting

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,11 +1,11 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     AWS_ACCESS_KEY_ID: str
     AWS_SECRET_ACCESS_KEY: str
     AWS_S3_BUCKET: str
-    
+
     class Config:
         env_file = ".env"
 
